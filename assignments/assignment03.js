1407c6e5120d6bc1e50d869c5b07fd22a3087c20
SilentGreenSeal <90408668+SilentGreenSeal@users.noreply.github.com>
2021-10-12 16:05:08 -0400

    Making  it loop

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -1,9 +1,10 @@
 $( document ).ready(function() {
 
 	let elem = [];
+	for(var i=1;i++;i<=18){
 	// assign the entire table row for hole 1 to a variable, elem
-	elem[1]
-	  = document.getElementById("1");
+	elem[i]
+	  = document.getElementById(i);
 
 	// display the number of children (all td elements)
 	// console.log(elem.children.length);
@@ -11,8 +12,9 @@ $( document ).ready(function() {
 	// console.log(elem.children[4].children[0]); 
 
 	// assign a function to the + button
-	elem[1].children[4].children[0].onclick 
+	elem[i].children[4].children[0].onclick 
 	  = function(){add1(elem[1]);};
+	}
 
 	// create an "add1" function
 	function add1 (elem) {
