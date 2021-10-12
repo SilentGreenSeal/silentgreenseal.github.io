23dc325649fa142921bc44036db63f72b7cadc90
SilentGreenSeal <90408668+SilentGreenSeal@users.noreply.github.com>
2021-10-12 16:54:05 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -2,18 +2,18 @@ $( document ).ready(function() {
 
 	let elem = [];
 	for(var i=0;i<18;i++){
-	// assign the entire table row for hole 1 to a variable, elem
-	elem[i]
-	  = document.getElementById(i+1);
+		// assign the entire table row for hole 1 to a variable, elem
+		elem[i]
+		  = document.getElementById(i+1);
 
-	// display the number of children (all td elements)
-	// console.log(elem.children.length);
-	// display the content of the + button, which is the first child of the fifth element
-	// console.log(elem.children[4].children[0]); 
+		// display the number of children (all td elements)
+		// console.log(elem.children.length);
+		// display the content of the + button, which is the first child of the fifth element
+		// console.log(elem.children[4].children[0]); 
 
-	// assign a function to the + button
-	elem[i].children[4].children[0].onclick 
-	  = function(){add1(elem[1]);};
+		// assign a function to the + button
+		elem[i].children[4].children[0].onclick 
+		  = function(){add1(elem[i]);};
 	}
 
 	// create an "add1" function
