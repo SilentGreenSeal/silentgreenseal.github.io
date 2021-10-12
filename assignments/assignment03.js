c7e9b15035921120d62166bb3b65d3ca762a6078
SilentGreenSeal <90408668+SilentGreenSeal@users.noreply.github.com>
2021-10-12 16:18:37 -0400

    Return to Array

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -3,7 +3,8 @@ $( document ).ready(function() {
 	let elem = [];
 	for(var i=1;i<=18;i++){
 	// assign the entire table row for hole 1 to a variable, elem
-	var obj = document.getElementById(i);
+	elem[i-1]
+	  = document.getElementById(i);
 
 	// display the number of children (all td elements)
 	// console.log(elem.children.length);
@@ -11,8 +12,8 @@ $( document ).ready(function() {
 	// console.log(elem.children[4].children[0]); 
 
 	// assign a function to the + button
-	obj.children[4].children[0].onclick 
-	  = function(){add1(obj);};
+	elem[i-1].children[4].children[0].onclick 
+	  = function(){add1(i-1);};
 	}
 
 	// create an "add1" function
