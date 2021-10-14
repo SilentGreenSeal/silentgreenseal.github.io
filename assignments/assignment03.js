f8feb516992f8178786aa7fe22fd217fa8815b0a
SilentGreenSeal <90408668+SilentGreenSeal@users.noreply.github.com>
2021-10-13 20:28:24 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	
 	for (var i=1;i>=18;i++) {
 		elem[i]
-			= document.getElementById(i);
+			= document.getElementById(i.toString());
 		elem[i].children[4].children[0].onclick 
 			= function(){add1(i.toString());};
 	}
