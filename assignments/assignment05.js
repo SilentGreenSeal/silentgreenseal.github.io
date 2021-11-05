4224594bd91c36fbc2591c8f986913a53e670e94
SilentGreenSeal <90408668+SilentGreenSeal@users.noreply.github.com>
2021-11-05 14:13:26 -0400

    Update assignment05.js

--- assignments/assignment05.js
+++ assignments/assignment05.js
@@ -243,7 +243,7 @@ function loadArray() {
 				break;
 			}
 		}
-		var pop = object[j][1];
+		var pop = populations[j][1];
 		var td = covidJsObj.Countries[i].TotalDeaths;
 	newArray.push({
 		"Slug": "\"" + covidJsObj.Countries[i].Slug + "\"",
