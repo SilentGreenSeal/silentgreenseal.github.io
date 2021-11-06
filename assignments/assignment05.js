e3b64156b69a5c3aa81afd4a43dc1ab0880259c1
SilentGreenSeal <90408668+SilentGreenSeal@users.noreply.github.com>
2021-11-06 19:57:24 -0400

    fix populations

--- assignments/assignment05.js
+++ assignments/assignment05.js
@@ -127,108 +127,107 @@ function loadContent() {
 } // end function loadContent() 
 
 // data from: https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population
-var populations = {
-  'china' : 1405137440,
-'india' : 1369152434,
-'united-states' : 330578332,
-'indonesia' : 269603400,
-'pakistan' : 220892331,
-'brazil' : 212281095,
-'nigeria' : 206139587,
-'bangladesh' : 169575884,
-'russia' : 146748590,
-'mexico' : 127792286,
-'japan' : 125880000,
-'philippines' : 109376023,
-'congo' : 101935800,
-'egypt' : 101126063,
-'ethiopia' : 100829000,
-'vietnam' : 96483981,
-'iran' : 83914898,
-'turkey' : 83154997,
-'germany' : 83122889,
-'france' : 67146000,
-'united-kingdom' : 66796807,
-'thailand' : 66571530,
-'italy' : 60026546,
-'south-africa' : 59622350,
-'tanzania' : 57637628,
-'myanmar' : 54817919,
-'south-korea' : 51841786,
-'colombia' : 50372424,
-'kenya' : 47564296,
-'spain' : 47329981,
-'argentina' : 45376763,
-'algeria' : 43900000,
-'sudan' : 42957030,
-'ukraine' : 41723998,
-'uganda' : 41583600,
-'iraq' : 40150200,
-'poland' : 38352000,
-'canada' : 38229409,
-'morocco' : 36063063,
-'uzbekistan' : 34501586,
-'saudi-arabia' : 34218169,
-'afghanistan' : 32890171,
-'malaysia' : 32703180,
-'peru' : 32625948,
-'angola' : 31127674,
-'ghana' : 30955202,
-'mozambique' : 30066648,
-'nepal' : 29996478,
-'yemen' : 29825968,
-'venezuela' : 28435943,
-'ivory-coast' : 26453542,
-'madagascar' : 26251309,
-'australia' : 25690614,
-'north-korea' : 25550000,
-'cameroon' : 24348251,
-'taiwan' : 23568378,
-'niger' : 23196002,
-'sri-lanka' : 21803000,
-'burkina-faso' : 21510181,
-'mali' : 20250833,
-'chile' : 19458310,
-'romania' : 19317984,
-'kazakhstan' : 18806296,
-'malawi' : 18449828,
-'zambia' : 17885422,
-'ecuador' : 17601388,
-'netherlands' : 17525931,
-'syria' : 17500657,
-'guatemala' : 16858333,
-'senegal' : 16705608,
-'chad' : 16244513,
-'somalia' : 15893219,
-'zimbabwe' : 15473818,
-'cambodia' : 15288489,
-'south-sudan' : 13249924,
-'rwanda' : 12663116,
-'guinea' : 12559623,
-'burundi' : 12309600,
-'benin' : 12114193,
-'haiti' : 11743017,
-'tunisia' : 11708370,
-'bolivia' : 11633371,
-'belgium' : 11539878,
-'cuba' : 11193470,
-'jordan' : 10804332,
-'greece' : 10724599,
-'czech-republic' : 10699142,
-'dominican-republic' : 10448499,
-'sweden' : 10367232,
-'portugal' : 10295909,
-'azerbaijan' : 10095900,
-'hungary' : 9769526,
-'belarus' : 9408400,
-'united-arab-emirates' : 9366829,
-'tajikistan' : 9313800,
-'honduras' : 9304380,
-'israel' : 9272700,
-'papua-new-guinea' : 8935000,
-'austria' : 8915382,
-'switzerland' : 8632703,
-}
+var populations=[];
+populations.push({name:'china',pop: 1405137440});
+populations.push({name:'india',pop:1369152434});
+populations.push({name:'united-states',pop:330578332});
+populations.push({name:'indonesia',pop:269603400});
+populations.push({name:'pakistan',pop:220892331});
+populations.push({name:'brazil',pop:212281095});
+populations.push({name:'nigeria',pop:206139587});
+populations.push({name:'bangladesh',pop:169575884});
+populations.push({name:'russia',pop:146748590});
+populations.push({name:'mexico',pop:127792286});
+populations.push({name:'japan',pop:125880000});
+populations.push({name:'philippines',pop:109376023});
+populations.push({name:'congo',pop:101935800});
+populations.push({name:'egypt',pop:101126063});
+populations.push({name:'ethiopia',pop:100829000});
+populations.push({name:'vietnam',pop:96483981});
+populations.push({name:'iran',pop:83914898});
+populations.push({name:'turkey',pop:83154997});
+populations.push({name:'germany',pop:83122889});
+populations.push({name:'france',pop:67146000});
+populations.push({name:'united-kingdom',pop:66796807});
+populations.push({name:'thailand',pop:66571530});
+populations.push({name:'italy',pop:60026546});
+populations.push({name:'south-africa',pop:59622350});
+populations.push({name:'tanzania',pop:57637628});
+populations.push({name:'myanmar',pop:54817919});
+populations.push({name:'south-korea',pop:51841786});
+populations.push({name:'colombia',pop:50372424});
+populations.push({name:'kenya',pop:47564296});
+populations.push({name:'spain',pop:47329981});
+populations.push({name:'argentina',pop:45376763});
+populations.push({name:'algeria',pop:43900000});
+populations.push({name:'sudan',pop:42957030});
+populations.push({name:'ukraine',pop:41723998});
+populations.push({name:'uganda',pop:41583600});
+populations.push({name:'iraq',pop:40150200});
+populations.push({name:'poland',pop:38352000});
+populations.push({name:'canada',pop:38229409});
+populations.push({name:'morocco',pop:36063063});
+populations.push({name:'uzbekistan',pop:34501586});
+populations.push({name:'saudi-arabia',pop:34218169});
+populations.push({name:'afghanistan',pop:32890171});
+populations.push({name:'malaysia',pop:32703180});
+populations.push({name:'peru',pop:32625948});
+populations.push({name:'angola',pop:31127674});
+populations.push({name:'ghana',pop:30955202});
+populations.push({name:'mozambique',pop:30066648});
+populations.push({name:'nepal',pop:29996478});
+populations.push({name:'yemen',pop:29825968});
+populations.push({name:'venezuela',pop:28435943});
+populations.push({name:'ivory-coast',pop:26453542});
+populations.push({name:'madagascar',pop:26251309});
+populations.push({name:'australia',pop:25690614});
+populations.push({name:'north-korea',pop:25550000});
+populations.push({name:'cameroon',pop:24348251});
+populations.push({name:'taiwan',pop:23568378});
+populations.push({name:'niger',pop:23196002});
+populations.push({name:'sri-lanka',pop:21803000});
+populations.push({name:'burkina-faso',pop:21510181});
+populations.push({name:'mali',pop:20250833});
+populations.push({name:'chile',pop:19458310});
+populations.push({name:'romania',pop:19317984});
+populations.push({name:'kazakhstan',pop:18806296});
+populations.push({name:'malawi',pop:18449828});
+populations.push({name:'zambia',pop:17885422});
+populations.push({name:'ecuador',pop:17601388});
+populations.push({name:'netherlands',pop:17525931});
+populations.push({name:'syria',pop:17500657});
+populations.push({name:'guatemala',pop:16858333});
+populations.push({name:'senegal',pop:16705608});
+populations.push({name:'chad',pop:16244513});
+populations.push({name:'somalia',pop:15893219});
+populations.push({name:'zimbabwe',pop:15473818});
+populations.push({name:'cambodia',pop:15288489});
+populations.push({name:'south-sudan',pop:13249924});
+populations.push({name:'rwanda',pop:12663116});
+populations.push({name:'guinea',pop:12559623});
+populations.push({name:'burundi',pop:12309600});
+populations.push({name:'benin',pop:12114193});
+populations.push({name:'haiti',pop:11743017});
+populations.push({name:'tunisia',pop:11708370});
+populations.push({name:'bolivia',pop:11633371});
+populations.push({name:'belgium',pop:11539878});
+populations.push({name:'cuba',pop:11193470});
+populations.push({name:'jordan',pop:10804332});
+populations.push({name:'greece',pop:10724599});
+populations.push({name:'czech-republic',pop:10699142});
+populations.push({name:'dominican-republic',pop:10448499});
+populations.push({name:'sweden',pop:10367232});
+populations.push({name:'portugal',pop:10295909});
+populations.push({name:'azerbaijan',pop:10095900});
+populations.push({name:'hungary',pop:9769526});
+populations.push({name:'belarus',pop:9408400});
+populations.push({name:'united-arab-emirates',pop:9366829});
+populations.push({name:'tajikistan',pop:9313800});
+populations.push({name:'honduras',pop:9304380});
+populations.push({name:'israel',pop:9272700});
+populations.push({name:'papua-new-guinea',pop:8935000});
+populations.push({name:'austria',pop:8915382});
+populations.push({name:'switzerland',pop:8632703});
 
 // step2 
 // new array 
@@ -239,7 +238,7 @@ function loadArray() {
 	for (let i=0; i<covidJsObj.Countries.length; i++) {
 		var toFind = covidJsObj.Countries[i].Slug;
 		for (var j = 0, len = populations.length; j < len; j++) {
-			if (populations[j][0].toLowerCase() === toFind.toLowerCase()) {
+			if (populations[j].name.toLowerCase() === toFind.toLowerCase()) {
 				break;
 			}
 		}
